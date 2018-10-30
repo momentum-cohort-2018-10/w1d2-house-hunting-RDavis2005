3bfb7fa4e030b24fa9fb708bb4b05299c4e0f960
RDavis2005 <RDavis2005@msn.com>
2018-10-30 12:38:57 -0400

    House Hunting Test Case 1

---
+++ househunting.py
@@ -0,0 +1,12 @@
+annual_salary = input("Enter your annual salary:")
+portion_saved = input("Enter the percent of your salary to save,as a decimal:")
+total_cost = input("Enter the cost of your dream home:")
+months = input("Number of months:")
+
+
+#portion_down_payment = 0.25
+#current_savings = 0
+#current_savings*r/1
+#r = 0.04
+
+#At the end of each month, your savings will be increased by the return on your investment, plus a percentage of your monthly salary ​(annual salary / 12)
