d97dc045c8995c73e335c5e3c1930dee3c462f17
RDavis2005 <RDavis2005@msn.com>
2018-10-30 14:01:48 -0400

    House Hunting

--- househunting.py
+++ househunting.py
@@ -1,12 +1,10 @@
 annual_salary = input("Enter your annual salary:")
+a = annual_salary
 portion_saved = input("Enter the percent of your salary to save,as a decimal:")
+b = portion_saved
 total_cost = input("Enter the cost of your dream home:")
-months = input("Number of months:")
+c = total_cost
 
-
-#portion_down_payment = 0.25
-#current_savings = 0
-#current_savings*r/1
-#r = 0.04
-
-#At the end of each month, your savings will be increased by the return on your investment, plus a percentage of your monthly salary ​(annual salary / 12)
+print("Your annual salary is:", (a))
+print("The percent of your salary to save is:", (b))
+print("The cost of your dream home is:", (c))
