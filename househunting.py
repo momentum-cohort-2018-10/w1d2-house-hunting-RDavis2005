412e0763ee63b8f818f5d42ef2391c875cec8d3e
RDavis2005 <RDavis2005@msn.com>
2018-10-31 07:32:04 -0400

    Hard Mode Completed

--- househunting.py
+++ househunting.py
@@ -1,10 +1,24 @@
-annual_salary = input("Enter your annual salary:")
-a = annual_salary
-portion_saved = input("Enter the percent of your salary to save,as a decimal:")
-b = portion_saved
-total_cost = input("Enter the cost of your dream home:")
-c = total_cost
+annual_salary = int(input("Enter your annual salary:"))
 
-print("Your annual salary is:", (a))
-print("The percent of your salary to save is:", (b))
-print("The cost of your dream home is:", (c))
+monthly = annual_salary / 12
+
+portion_saved = float(input("Enter the percent of your salary to save,as a decimal:"))
+
+annual_rate_of_return = float(input("Enter the expected annual rate of return:"))
+
+total_cost = int(input("Enter the cost of your dream home:"))
+
+percent_to_save = float(input("Enter the percent of your home's cost to save as a down payment:"))
+
+current_savings = 0
+month = 0
+r = annual_rate_of_return / 12
+monthly_return = monthly * r
+
+down_payment = int(total_cost * percent_to_save)
+
+while current_savings <= down_payment:
+    month = month + 1
+    current_savings += (current_savings * r) + (portion_saved * monthly)
+    print(month)
+print("It will take you", (month), "months to have enough money for the down payment.")
